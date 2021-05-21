e4ea9ccc2eba227eaf5ef8953b22a94619c81a8f
zheng-qipeng <50936043+zheng-qipeng@users.noreply.github.com>
2021-05-21 06:19:42 +0000

    Deploying to gh-pages from @ zheng-qipeng/modem-bookmarks@5834e317c903be24db666d835afb1013dacddbf8 🚀

--- js/copy.js
+++ js/copy.js
@@ -1,11 +1,11 @@
 export default () => {
   function addCopy(e) {
-    let txt = ""
+    let copyTxt = ""
     e.preventDefault() // 取消默认的复制事件
-    const copyText = window.getSelection().toString()
-    txt = `${copyText}\n作者：zhengqipeng\n原文：${window.location.href}\n著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。`
+    copyTxt = window.getSelection(0).toString()
+    copyTxt = `${copyTxt}\n作者：zhengqipeng\n原文：${window.location.href}\n著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。`
     const clipboardData = e.clipboardData || window.clipboardData
-    clipboardData.setData('text', txt)
+    clipboardData.setData('text', copyTxt);
   }
   document.addEventListener("cut", e => {
     addCopy(e)
