00832f318aac841c86b7dffdb9f1ab8265bf7989
zhengqipeng <zhengqipeng@szeastroc.com>
2021-06-03 16:47:11 +0800

    refactor canvas-nest

--- docs/.vuepress/public/js/canvas.js
+++ docs/.vuepress/public/js/canvas.js
@@ -1,3 +1,5 @@
+import CanvasNest from "canvas-nest.js";
+
 export default () => {
     // console.log(window.screen.width * window.devicePixelRatio)
     function IsPC () {
@@ -13,16 +15,13 @@ export default () => {
         return flag
     }
     if (IsPC()) {
-        let script = document.createElement('script')
-        script.type = 'text/javascript'
-        script.src = "/modem-bookmarks/js/canvas-nest.js"
-        // script.setAttribute('color', '0,0,0')
-        script.setAttribute('opacity', '0.7')
-        script.setAttribute('zIndex', '10')
-        script.setAttribute('count', '18')
-        document.body.appendChild(script)
-        setTimeout(function () {
-            document.getElementsByTagName('canvas')[document.getElementsByTagName('canvas').length - 1].style.pointerEvents = 'none'
-        }, 1000)
+        new CanvasNest(document.body, {
+            color: '0,0,0',
+            pointColor: '0,0,0',
+            opacity: '0.8',
+            count: 18,
+            zIndex: 10,
+        });
+        document.body.style.height = '100vh'
     }
 }
