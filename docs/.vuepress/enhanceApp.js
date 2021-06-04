fc0ec1fcad5069232545c86f2cc3d3107cdd371c
zhengqipeng <zhengqipeng@szeastroc.com>
2021-06-04 11:20:50 +0800

    fix 编译报错

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -8,17 +8,17 @@ export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
   router, // 当前应用的路由实例
-  siteData // 站点元数据
+  siteData, // 站点元数据
+  isServer // 当前应用配置是处于 服务端渲染 或 客户端
 }) => {
   // ...做一些其他的应用级别的优化
   Vue.use(Element)
-  try {
-    document && (() => { //对document的判断是防止编译的时候报错
-      documentTitle()
-      canvas()
-      copy()
-    })()
+
+  try { // 防止编译的时候报错
+    documentTitle()
+    canvas()
+    copy()
   } catch (e) {
-    console.error(e.message, '???')
+    // console.error(e.message, '???')
   }
 }
